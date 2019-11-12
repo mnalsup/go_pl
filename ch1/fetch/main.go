36bf38a7b0558a2eb58bc2205d78e7d5f6c782ef
Alsup <matthew.alsup@asurion.com>
2019-11-12 11:01:44 -0600

    exercises 1.7 - 1.9

--- ch1/fetch/main.go
+++ ch1/fetch/main.go
@@ -2,24 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = fmt.Sprintf("http://%s", url)
+		}
 		resp, err := http.Get(url)
-		defer resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		fmt.Printf("Status: %s\n", resp.Status)
+		_, err = io.Copy(os.Stdout, resp.Body)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
 	}
 }
